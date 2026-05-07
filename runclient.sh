be90bb3ba7136af6cde96207396fb6e9c8e2c61d
ilikeships <105243738+ilikeships@users.noreply.github.com>
2026-05-07 23:28:57 +0300

    f

--- runclient.sh
+++ runclient.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dotnet run --project Content.Client
+dotnet run -c Release -p Content.Client
 read -p "Press enter to continue"
--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dotnet run --project Content.Server
+dotnet run -c Release -p Content.Server
 read -p "Press enter to continue"

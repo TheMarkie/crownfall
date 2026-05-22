62a0d8b5ab7f77afb03d16d436c8cf1b8a860de5
ilikeships <105243738+ilikeships@users.noreply.github.com>
2026-05-22 22:06:36 +0300

    Revert "ported spacebiomes"

--- Content.Server/Parallax/ParallaxSystem.cs
+++ Content.Server/Parallax/ParallaxSystem.cs
@@ -2,7 +2,4 @@ using Content.Shared.Parallax;
 
 namespace Content.Server.Parallax;
 
-public sealed class ParallaxSystem : SharedParallaxSystem
-{
-    // TODO: PORT SWAPPARALLAX FROM HULLROT
-}
+public sealed class ParallaxSystem : SharedParallaxSystem { }
